9f9b07350c79e6bfa4c14429c46997e2207d4a4c
System Administrator <root@MacBook-Air.fibertel.com.ar>
2023-07-31 20:48:54 -0300

    about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,55 @@
 import React from "react";
 import "./About.css";
+import ME from "../../assets/ME.png";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me_img">
+            <img src={ME} alt="About me" />
+          </div>
+        </div>
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_icon" />
+              <h5>Experience</h5>
+              <small>2+ Years Working</small>
+            </article>
+
+            <article className="about_card">
+              <FiUsers className="about_icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed Projects</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum
+            illum est ex accusamus quam neque in et ullam at voluptatem quod,
+            cum deserunt nisi quasi corporis sit ipsa assumenda aperiam!
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
